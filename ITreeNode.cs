af7d294862b49aab014d1998b21eb4da6100102d
sergiosssg <sergiosssg@gmail.com>
2021-09-01 12:46:54 +0300

    created new public interface   ITreeNode<IElementOfTree> ,  with two public methods :         =-  public bool AmIleaf()         =-  public IElementOfTree GetCurrentElement().

---
+++ ITreeNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeLib
+{
+    public interface ITreeNode<IElementOfTree>
+    {
+        public bool AmIleaf();
+
+        public IElementOfTree GetCurrentElement();
+    }
+}
