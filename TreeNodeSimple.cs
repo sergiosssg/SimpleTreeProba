2f66c73e2bd7af2425432592473bf1836ede6d36
sergiosssg <sergiosssg@gmail.com>
2021-09-03 09:18:25 +0300

    Created new public class  TreeNodeSimple<T>,  so it implements  ITreeNode<T>,        where T : IElementOfTreeContent.

---
+++ TreeNodeSimple.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeLib
+{
+    public class TreeNodeSimple<T> : ITreeNode<T> where T : IElementOfTreeContent
+    {
+
+
+        public bool AmIleaf() => true;
+
+
+        public IEnumerable<ITreeNode<T>> GetChildren()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetCurrentElement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITreeNode<T> GetParent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
