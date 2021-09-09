5ccdc714a98638690a99bcd6c3f94c16ae85bf0c
sergiosssg <sergiosssg@gmail.com>
2021-09-09 16:22:03 +0300

    Created new public struct  GoalOfSearchInTree ,     =-  public struct GoalOfSearchInTree<I> where I : IElementOfTreeContent,               with  members :     =-  public TypeOfComparingStrategyOfTreeNode typeOfComparingStrategyOfTreeNode,     =-  public ITreeNode<I> treeNode,     =-  public I nodeContent,     =-  public int depthLevel,     =-  public int siblingOrder.

---
+++ GoalOfSearchInTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeLib
+{
+    public struct GoalOfSearchInTree<I> where I : IElementOfTreeContent
+    {
+        public TypeOfComparingStrategyOfTreeNode typeOfComparingStrategyOfTreeNode;
+        public ITreeNode<I> treeNode;
+        public I nodeContent;
+        public int depthLevel;
+        public int siblingOrder;
+    }
+}
