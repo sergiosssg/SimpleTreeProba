9db421f05e0b226d7dc0fca4616bcc0367fce15e
sergiosssg <sergiosssg@gmail.com>
2021-09-10 11:49:04 +0300

    Created new public class ResultOfSearchInTree, for fetching results from searching in tree proper node :     =-  public class ResultOfSearchInTree<I> where I : IElementOfTreeContent,              with  private variable members :  =-  private bool _foundResult,     =-  private bool _founndByContent,     =-  private I _foundElementOfTreeContent,     =-  private bool _founndByTreeNode,     =-  private ITreeNode<I> _foundTreeNode,     =-  private bool _foundByThopology,     =-  private int _depthLevel,     =-  private int siblingOrder.

---
+++ ResultOfSearchInTree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeLib
+{
+    public class ResultOfSearchInTree<I> where I : IElementOfTreeContent
+    {
+        private bool _foundResult;
+        private bool _founndByContent;
+        private I _foundElementOfTreeContent;
+        private bool _founndByTreeNode;
+        private ITreeNode<I> _foundTreeNode;
+        private bool _foundByThopology;
+        private int _depthLevel;
+        private int siblingOrder;
+
+    }
+}
