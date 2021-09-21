6e023c1f51e947d0f28ba264f9ebc887c52fcbdc
sergiosssg <sergiosssg@gmail.com>
2021-09-21 15:17:34 +0300

    Added comments describing  delegate  PredicateComparingTreeNodeAndSmple.

--- ITraverser.cs
+++ ITraverser.cs
@@ -4,6 +4,14 @@ using System.Text;
 
 namespace TreeLib
 {
+    /// <summary>
+    ///   This predicate compare sample with given node of tree, if comparing true, predicate should return true,
+    ///   and false otherwise
+    /// </summary>
+    /// <typeparam name="T">  any object type implemented  IElementOfTreeContent interface </typeparam>
+    /// <param name="treeNodeForComparing"> node with which we compare sample</param>
+    /// <param name="goalOfSearch"> sample is to one node should be compared</param>
+    /// <returns> true , when first argument is equal to sample , false otherwise </returns>
     public delegate bool PredicateComparingTreeNodeAndSmple<T> (
                        in ITreeNode<T> treeNodeForComparing,
                        GoalOfSearchInTree<T> goalOfSearch) where T : IElementOfTreeContent;
