d9784a009abd750d22e1341f2a034087d3d479ff
sergiosssg <sergiosssg@gmail.com>
2021-09-23 15:50:47 +0300

    Created  public  class  TreeTraversingContext,               full signature :     =-  public class TreeTraversingContext<T> where T : IElementOfTreeContent               with following properties :     =-  public bool BacktrackingToggle     =-  public Stack<ITreeNode<T>> UntouchedNodes     =-  public Stack<ITreeNode<T>> TouchedNodes     =-  public ITree<T> CurrentTree     =-  public bool IsTreeConsistent     =-  public ITreeNode<T> CurrentTreeNode     =-  public bool IsOrderInWidthDefined     =-  public int OrderInWidth     =-  public bool IsLevelInDepthDefined     =-  public  int LevelInDepth               and  methods :     =-  public bool ResetContext()     =-  public bool ResetLevelInDepth()     =-  public bool ResetOrderInWidth().

---
+++ TreeTraversingContext.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeLib
+{
+    public class TreeTraversingContext<T> where T : IElementOfTreeContent
+    {
+
+        #region Properties
+        /// <summary>
+        /// Backtracking Toggle Switch
+        ///  When is true,
+        ///  Should accomplish backtracking movement so double goaround every node in tree
+        ///  and to have use Untouched nodes
+        /// </summary>
+        public bool BacktrackingToggle
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Stack<ITreeNode<T>> UntouchedNodes
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Stack<ITreeNode<T>> TouchedNodes
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ITree<T> CurrentTree
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsTreeConsistent
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ITreeNode<T> CurrentTreeNode
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsOrderInWidthDefined
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int OrderInWidth
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsLevelInDepthDefined
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public  int LevelInDepth
+        {
+            get;
+        }
+        #endregion
+
+
+        #region Public Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool ResetContext()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool ResetLevelInDepth()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool ResetOrderInWidth()
+        {
+            return true;
+        }
+        #endregion
+    }
+}
